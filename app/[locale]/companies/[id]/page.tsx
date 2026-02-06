53267f609be454650397d98e9845d433c6028740
tianshanxyz <tianshanxyz@gmail.com>
2026-02-06 21:34:22 +0800

    Fix JSX syntax error: add missing : null in ternary operator

--- app/[locale]/companies/[id]/page.tsx
+++ app/[locale]/companies/[id]/page.tsx
@@ -383,7 +383,7 @@ export default function CompanyDetailPage({
                     ))}
                   </div>
                 </div>
-              )}
+              ) : null}
             </div>
           )}
 
